c119044c170fc34db3c66832ae092972dca63b17
Iaritzza Bielinki <iaritzzayara@gmail.com>
2021-09-03 08:48:20 -0300

    Projeto criado

---
+++ ValidacaoDados/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacaoDados.Models
+{
+    public class Contexto : DbContext 
+    {
+        public DbSet<Pessoa> pessoas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base (opcoes)
+        {
+        }
+    }
+}
---
+++ ValidacaoDados/Models/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacaoDados.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Range (1,100, ErrorMessage = "Você não é velho assim! Digite um valor válido")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        [Compare("Email", ErrorMessage = "Não válido")]
+        public string ConfirmarEmail { get; set; }
+    }
+}
